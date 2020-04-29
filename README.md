6c1b4d1e5c328c5d22b8f055d41a57ec2e9b921e
Qianxiong Xu <20086454@mail.wit.ie>
2020-04-29 12:03:43 +0100

    Update README

--- README.md
+++ README.md
@@ -17,10 +17,13 @@ Deploy the AI models to AWS for the final year project Custom Outfit App.
 - OS: Ubuntu 16.04
 - AMI: Deep Learning AMI (Ubuntu 16.04) Version 27.0 64-bit(x86)
 - Instance: AWS g3s.xlarge
+    - When creating the instance, you can let all traffic in for convenience; if not, open 80, 443 ports.
 - vCPUs: 4
 - Memory: 30.5GB
 - GPU: NVIDIA Tesla M60 (8GB)
 - Storage: 300GB
+- Docker: v19.03.6
+- Docker-compose: v1.26.0-rc3
 
 ## Usage
 - Clone:
@@ -35,11 +38,69 @@ $ cd ..
 ```
 $ sh download_models.sh
 ```
+- (optional) Setup a nginx server and config for `https` (if you need to use https for `production usage`):
+    - Purchase a domain, e.g. from [Freenom](https://www.freenom.com/en/index.html?lang=en).
+    - Request a elastic ip from AWS and bind to the current instance.
+    - Bind the elastic ip to your domain in Freenom (the config will take about 1-2 days to take effects).
+    - Install `certbot`:
+```
+$ sudo apt-get install software-properties-common
+$ sudo add-apt-repository ppa:certbot/certbot
+$ sudo apt-get update
+$ sudo apt-get install certbot
+```
+    - Generate `certificate`, here, I use my own domain as an example, please replace them by your own domain:
+```
+$ sudo certbot certonly --standalone -d xusam2412.ml -d www.xusam2412.ml
+$ sudo ls /etc/letsencrypt/live/
+(you can find the generated keys here)
+```
+    - Install and `Nginx`:
+```
+$ sudo apt-get install nginx
+$ sudo vim /etc/nginx/sites-available/default
+1. find the row containing 'listen [::]:80 default_server;'.
+2. modify the row as 'listen [::]:80 ipv6only=on default_server;'.
+(if not, the nginx service may not start up normally due to the confliction of port used)
+
+$ sudo vim /etc/nginx/nginx.conf
+1. add the following script inside the 'http' block, you should replace the ssl_certificate and ssl_certificate_key values with your own files' paths.
+server {
+  listen 443;
+  server_name xusam2412.ml www.xusam2412.ml;
+  ssl on;
+  ssl_certificate /etc/letsencrypt/live/xusam2412.ml/fullchain.pem;
+  ssl_certificate_key /etc/letsencrypt/live/xusam2412.ml/privkey.pem;
+  location /api_server {
+    proxy_pass http://0.0.0.0:5000/api_server;
+    proxy_http_version 1.1;
+    proxy_set_header X_FORWARDED_PROTO https;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header Host $host;
+  }
+  location /mmfashion {
+    proxy_pass http://0.0.0.0:5012/mmfashion;
+    proxy_http_version 1.1;
+    proxy_set_header X_FORWARDED_PROTO https;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header Host $host;
+  }
+}
+
+$ sudo service nginx restart
+```
 - Build docker containers and images:
 ```
 $ docker-compose up -d
-(1st time setup takes a long period)
-(the flask server needs some time to initialize)
+1. 1st time setup takes a long period to create the containers and images.
+
+$ watch -n 1 nvidia-smi
+1. once the containers and images are built, it still needs some time to start up because the flask server needs some time to initialize. A tip is using this command to monitor the gpu memory usage to judge if the initialize is done.
+2. when the process of initializing finishing, the gpu memory usage will be about 2700MiB.
 ```
 
 ## References
--- api_server/app.py
+++ api_server/app.py
@@ -18,40 +18,30 @@ from flask_cors import CORS
 import torch
 import torch.optim as optim
 
-# 自作モジュール
+# Self defined module
 from utils import conv_base64_to_pillow, conv_pillow_to_base64, conv_tensor_to_pillow, create_binary_mask
 from utils import save_checkpoint, load_checkpoint
 from networks import GMM, End2EndGenerator
 from dataset import VtonDataset, VtonDataLoader
 
 #======================
-# グローバル変数
+# Global varaible
 #======================
 args = None
 
 #-----------------
-# flask 関連
+# flask
 #-----------------
 app = flask.Flask(__name__)
 
-# OPTIONS を受け取らないようにする（Access-Control-Allow-Origin エラー対策）
+# Access-Control-Allow-Origin Policy
 CORS(app, resources={r"*": {"origins": "*"}}, methods=['POST', 'GET'])
 
-app.config['JSON_AS_ASCII'] = False     # 日本語文字化け対策
-app.config["JSON_SORT_KEYS"] = False    # ソートをそのまま
+app.config['JSON_AS_ASCII'] = False
+app.config["JSON_SORT_KEYS"] = False
 
 #-----------------
-# OpenPose 関連
-#-----------------
-#openpose_server_url = http://openpose_ubuntu_gpu_container:5010/openpose
-
-#-----------------
-# Graphonomy 関連
-#-----------------
-#graphonomy_server_url = http://graphonomy_server_gpu_container:5001/graphonomy
-
-#-----------------
-# 試着モデル関連
+# Virtual Try-on related variables
 #-----------------
 device = None
 model_G = None
@@ -59,30 +49,27 @@ ds_test = None
 dloader_test = None
 
 #================================================================
-# "http://host_ip:5000" リクエスト送信（＝ページにアクセス）時の処理
+# "http://host_ip:5000"
 #================================================================
 @app.route('/')
 def index():
-    return flask.render_template('index.html', title='virtual-try-on_webapi_flask') 
+    return flask.render_template('index.html', title='virtual-try-on_webapi_flask')
 
 #================================================================
-# "http://host_ip:5000/api_server" にリクエスト送信時の処理
+# "http://host_ip:5000/api_server"
 #================================================================
-#@app.route('/api_server', methods=['POST','OPTIONS'])
 @app.route('/api_server', methods=['POST'])
 def responce():
-    print( "リクエスト受け取り" )
     if( app.debug ):
         print( "flask.request.method : ", flask.request.method )
         print( "flask.request.headers \n: ", flask.request.headers )
 
-    # ブラウザから送信された json データの取得
     if( flask.request.headers["User-Agent"].split("/")[0] in "python-requests" ):
         json_data = json.loads(flask.request.json)
     else:
         json_data = flask.request.get_json()
 
-    #
+    # file paths
     poseA_img_path = os.path.join( args.dataset_dir, "test", "poseA", "1.png" )
     poseB_img_path = os.path.join( args.dataset_dir, "test", "poseB", "1.png" )
     cloth_img_path = os.path.join( args.dataset_dir, "test", "cloth", "1.png" )
@@ -91,7 +78,7 @@ def responce():
     poseB_keypoints_path = os.path.join( args.dataset_dir, "test", "poseB_keypoints", "1_keypoints.json" )
 
     #------------------------------------------
-    # ブラウザから送信された画像データの変換
+    # imgs: base64 -> rgb format
     #------------------------------------------
     pose_img_pillow = conv_base64_to_pillow( json_data["pose_img_base64"] )
     cloth_img_pillow = conv_base64_to_pillow( json_data["cloth_img_base64"] )
@@ -105,16 +92,16 @@ def responce():
     cloth_img_pillow.save( cloth_img_path )
 
     #------------------------------------------
-    # Graphonomy を用いて人物パース画像を生成する。
+    # Graphonomy: Human parsing (seperate each part, e.g. head, body, hand, etc.)
     #------------------------------------------
     graphonomy_msg = {'pose_img_base64': pose_img_base64 }
-    graphonomy_msg = json.dumps(graphonomy_msg)     # dict を JSON 文字列として整形して出力
+    graphonomy_msg = json.dumps(graphonomy_msg)
     try:
         graphonomy_responce = requests.post( args.graphonomy_server_url, json=graphonomy_msg )
         graphonomy_responce = graphonomy_responce.json()
 
     except Exception as e:
-        print( "通信失敗 [Graphonomy]" )
+        print( "Fail to connect to the graphonomy server." )
         print( "Exception : ", e )
         #torch.cuda.empty_cache()
 
@@ -137,9 +124,9 @@ def responce():
     pose_parse_img_RGB_pillow.save( os.path.join( args.dataset_dir, "test", "poseB_parsing", "1_vis.png" ) )
 
     #------------------------------------------
-    # OpenPose を用いて人物姿勢 keypoints を生成する。
+    # OpenPose: Extract keypoints of human pose
     #------------------------------------------
-    # request 
+    # request
     oepnpose_msg = {'pose_img_base64': pose_img_base64 }
     oepnpose_msg = json.dumps(oepnpose_msg)
     try:
@@ -151,7 +138,7 @@ def responce():
             json.dump( openpose_responce, f, ensure_ascii=False )
 
     except Exception as e:
-        print( "通信失敗 [OpenPose]" )
+        print( "Fail to connect to the openpose server." )
         print( "Exception : ", e )
         #torch.cuda.empty_cache()
 
@@ -164,16 +151,16 @@ def responce():
         return http_status_code, response
 
     #------------------------------------------
-    # 前処理
+    # Preprocessing
     #------------------------------------------
-    # 服マスク画像を生成する。
+    # Generate cloth mask
     cloth_mask_img_cv = create_binary_mask( cloth_img_path )
     cv2.imwrite( cloth_mask_img_path, cloth_mask_img_cv )
 
     #------------------------------------------
-    # 試着モデルから試着画像を生成する。
+    # virtual try-on
     #------------------------------------------
-    # ミニバッチデータを GPU へ転送
+    # Put a small batch of data onto gpu for calculating
     inputs = dloader_test.next_batch()
     cloth_tsr = inputs["cloth_tsr"].to(device)
     cloth_mask_tsr = inputs["cloth_mask_tsr"].to(device)
@@ -206,7 +193,7 @@ def responce():
         poseB_warp_cloth, poseB_warp_cloth_mask, poseB_warped_grid, \
         poseA_rough, poseA_attention, poseA_gen, \
         poseB_rough, poseB_attention, poseB_gen \
-        = model_G( 
+        = model_G(
             cloth_tsr, cloth_mask_tsr, grid_tsr,
             poseA_tsr, poseA_bodyshape_mask_tsr, poseA_gmm_agnostic_tsr, poseA_tom_agnostic_tsr, poseA_keypoints_tsr, poseA_wuton_agnotic_tsr,
             poseB_tsr, poseB_bodyshape_mask_tsr, poseB_gmm_agnostic_tsr, poseB_tom_agnostic_tsr, poseB_keypoints_tsr, poseB_wuton_agnotic_tsr,
@@ -221,7 +208,7 @@ def responce():
     tryon_img_base64 = conv_pillow_to_base64( tryon_img_pillow )
 
     #------------------------------------------
-    # json 形式のレスポンスメッセージを作成
+    # Send generated image back in the format of base64
     #------------------------------------------
     #torch.cuda.empty_cache()
     http_status_code = 200
@@ -231,8 +218,8 @@ def responce():
             'tryon_img_base64': tryon_img_base64,
         }
     )
-    
-    # レスポンスメッセージにヘッダーを付与（Access-Control-Allow-Origin エラー対策）
+
+    # Cross-Domain Policy
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     #response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
@@ -244,73 +231,65 @@ def responce():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    #parser.add_argument('--host', type=str, default="localhost", help="IP アドレス / 0.0.0.0 でどこからでもアクセス可")
-    parser.add_argument('--host', type=str, default="0.0.0.0", help="IP アドレス / 0.0.0.0 でどこからでもアクセス可")
-    parser.add_argument('--port', type=str, default="5000", help="ポート番号")
-    parser.add_argument('--enable_threaded', action='store_true', help="並列処理有効化")
+    parser.add_argument('--host', type=str, default="0.0.0.0", help="IP - 0.0.0.0 by default")
+    parser.add_argument('--port', type=str, default="5000", help="port number")
+    parser.add_argument('--enable_threaded', action='store_true', help="if multi-threaded")
 
-    parser.add_argument('--openpose_server_url', type=str, default="http://openpose_ubuntu_gpu_container:5010/openpose", help="OpenPose サーバーの URL")
-    parser.add_argument('--graphonomy_server_url', type=str, default="http://graphonomy_server_gpu_container:5001/graphonomy", help="Graphonomy サーバーの URL")
-    #parser.add_argument('--openpose_server_container_name', type=str, default="openpose_ubuntu_gpu_container", help="OpenPose サーバーのコンテナ名")
-    #parser.add_argument('--openpose_server_port', type=str, default="5010", help="OpenPose サーバーのポート番号")
-    #parser.add_argument('--graphonomy_server_container_name', type=str, default="graphonomy_server_gpu_container", help="OpenPose サーバーのコンテナ名")
-    #parser.add_argument('--graphonomy_server_port', type=str, default="5001", help="OpenPose サーバーのポート番号")
+    parser.add_argument('--openpose_server_url', type=str, default="http://openpose_ubuntu_gpu_container:5010/openpose", help="OpenPose Server URL")
+    parser.add_argument('--graphonomy_server_url', type=str, default="http://graphonomy_server_gpu_container:5001/graphonomy", help="Graphonomy Server URL")
 
-    parser.add_argument('--device', choices=['cpu', 'gpu'], default="gpu", help="使用デバイス (CPU or GPU)")
-    parser.add_argument('--dataset_dir', type=str, default="datasets", help="データセットのディレクトリ")
-    parser.add_argument('--pair_list_path', type=str, default="datasets/test_pairs.csv", help="ペアリストファイル名")
-    parser.add_argument('--load_checkpoints_gmm_path', type=str, default="checkpoints/improved_cp-vton_train_end2end_zalando_vton_dataset1_256_use_tom_agnotic_200215/GMM/gmm_final.pth", help="GMMモデルの読み込みファイルのパス")
-    parser.add_argument('--load_checkpoints_tom_path', type=str, default="checkpoints/my-vton_train_end2end_zalando_vton_dataset2_256_200218/TOM/tom_final.pth", help="TOMモデルの読み込みファイルのパス")
-    parser.add_argument('--batch_size', type=int, default=1, help="バッチサイズ")
-    parser.add_argument('--image_height', type=int, default=256, help="入力画像の高さ（pixel単位）")
-    parser.add_argument('--image_width', type=int, default=192, help="入力画像の幅（pixel単位）")
+    parser.add_argument('--device', choices=['cpu', 'gpu'], default="gpu", help="use (CPU or GPU)")
+    parser.add_argument('--dataset_dir', type=str, default="datasets", help="dataset directory")
+    parser.add_argument('--pair_list_path', type=str, default="datasets/test_pairs.csv", help="pair list file for testing")
+    parser.add_argument('--load_checkpoints_gmm_path', type=str, default="checkpoints/improved_cp-vton_train_end2end_zalando_vton_dataset1_256_use_tom_agnotic_200215/GMM/gmm_final.pth", help="GMM model path")
+    parser.add_argument('--load_checkpoints_tom_path', type=str, default="checkpoints/my-vton_train_end2end_zalando_vton_dataset2_256_200218/TOM/tom_final.pth", help="TOM model path")
+    parser.add_argument('--batch_size', type=int, default=1, help="batch size")
+    parser.add_argument('--image_height', type=int, default=256, help="image height")
+    parser.add_argument('--image_width', type=int, default=192, help="image width")
     parser.add_argument('--grid_size', type=int, default=5)
 
     parser.add_argument('--poseA_bodyshape_downsampling_size', type=int, default=4)
     parser.add_argument('--poseB_bodyshape_downsampling_size', type=int, default=4)
-    parser.add_argument('--gmm_agnostic_type', choices=['agnostic1', 'agnostic2', 'agnostic3'], default="agnostic1", help="GMM agnotic の形状タイプ")
-    parser.add_argument('--tom_agnostic_type', choices=['agnostic1', 'agnostic2', 'agnostic3'], default="agnostic2", help="TOM agnotic の形状タイプ")
-    parser.add_argument('--use_tom_wuton_agnotic', action='store_true', help="WUTON 形式の agnotic 入力を使用するか否か")
-    parser.add_argument('--wuton_agnotic_kernel_size', type=int, default=6, help="WUTON 形式の agnotic 入力の膨張カーネルサイズ")
-    parser.add_argument('--reuse_tom_wuton_agnotic', action='store_true', help="WUTON 形式の agnotic 入力を再利用するか否か")
-    parser.add_argument('--eval_poseA_or_poseB', choices=['poseA', 'poseB'], default="poseB", help="人物画像の推論対象")
+    parser.add_argument('--gmm_agnostic_type', choices=['agnostic1', 'agnostic2', 'agnostic3'], default="agnostic1", help="GMM agnotic shape type")
+    parser.add_argument('--tom_agnostic_type', choices=['agnostic1', 'agnostic2', 'agnostic3'], default="agnostic2", help="TOM agnotic shape type")
+    parser.add_argument('--use_tom_wuton_agnotic', action='store_true', help="if use WUTON shape agnotic as input")
+    parser.add_argument('--wuton_agnotic_kernel_size', type=int, default=6, help="increase kernel size for WUTON shape agnotic")
+    parser.add_argument('--reuse_tom_wuton_agnotic', action='store_true', help="if reuse WUTON shape agnotic")
+    parser.add_argument('--eval_poseA_or_poseB', choices=['poseA', 'poseB'], default="poseB", help="target")
 
-    parser.add_argument('--use_amp', action='store_true', help="AMP [Automatic Mixed Precision] の使用有効化")
+    parser.add_argument('--use_amp', action='store_true', help="use AMP [Automatic Mixed Precision]")
     parser.add_argument('--opt_level', choices=['O0','O1','O2','O3'], default='O1', help='mixed precision calculation mode')
-    parser.add_argument("--seed", type=int, default=8, help="乱数シード値")
-    parser.add_argument('--debug', action='store_true', help="デバッグモード有効化")
+    parser.add_argument("--seed", type=int, default=8, help="random seed")
+    parser.add_argument('--debug', action='store_true', help="if debug")
     args = parser.parse_args()
     if( args.debug ):
         for key, value in vars(args).items():
             print('%s: %s' % (str(key), str(value)))
 
     #------------------------------------
-    # グローバル変数の設定
+    # Set global variable
     #------------------------------------
     args = args
-    #openpose_server_url = "http://" + args.openpose_server_container_name + ":" + args.openpose_server_port + "/openpose"
-    #graphonomy_server_url = "http://" + args.graphonomy_server_container_name + ":" + args.graphonomy_server_port + "/graphonomy"
 
     #------------------------------------
-    # 実行 Device の設定
+    # Set device: cpu/gpu
     #------------------------------------
     if( args.device == "gpu" ):
         use_cuda = torch.cuda.is_available()
         if( use_cuda == True ):
             device = torch.device( "cuda" )
-            #torch.cuda.set_device(args.gpu_ids[0])
-            print( "実行デバイス :", device)
-            print( "GPU名 :", torch.cuda.get_device_name(device))
+            print( "device :", device)
+            print( "GPU :", torch.cuda.get_device_name(device))
             print("torch.cuda.current_device() =", torch.cuda.current_device())
         else:
             print( "can't using gpu." )
             device = torch.device( "cpu" )
-            print( "実行デバイス :", device)
+            print( "device :", device)
     else:
         device = torch.device( "cpu" )
-        print( "実行デバイス :", device)
+        print( "device :", device)
 
-    # seed 値の固定
+    # set random seed
     np.random.seed(args.seed)
     random.seed(args.seed)
     torch.manual_seed(args.seed)
@@ -318,7 +297,7 @@ if __name__ == "__main__":
     torch.cuda.manual_seed_all(args.seed)
 
     #------------------------------------
-    # 試着モデル
+    # Load Virtual Try-on model
     #------------------------------------
     model_G = End2EndGenerator( args, device, use_cuda=True ).to(device)
     if not args.load_checkpoints_gmm_path == '' and os.path.exists(args.load_checkpoints_gmm_path):
@@ -329,16 +308,16 @@ if __name__ == "__main__":
     model_G.eval()
 
     #-------------------------------
-    # AMP の適用（使用メモリ削減効果）
+    # if use AMP (it can decrease the usage of memory)
     #-------------------------------
     if( args.use_amp ):
-        # dummy の optimizer
+        # dummy optimizer
         optimizer = optim.Adam( params = model_G.parameters(), lr = 0.0001, betas = (0.5,0.999) )
 
         # amp initialize
         model_G, optimizer = amp.initialize(
-            model_G, 
-            optimizer, 
+            model_G,
+            optimizer,
             opt_level = args.opt_level,
             num_losses = 1
         )
@@ -350,7 +329,7 @@ if __name__ == "__main__":
     dloader_test = VtonDataLoader(ds_test, batch_size=args.batch_size, shuffle=False )
 
     #------------------------------------
-    # Flask の起動
+    # Start Flask
     #------------------------------------
     app.debug = args.debug
     if( args.enable_threaded ):
